f87c1413c650e3e3a42a07daa069b63c423bf22e
Andrew Letson <velinath@users.noreply.github.com>
2017-05-09 22:57:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ client.on('message', message => {
 });
 
 client.login(process.env.app_token);
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.send('it is running\n'); }).listen(process.env.PORT || 5000);
+});
