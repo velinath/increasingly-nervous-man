11c794741f7bfc8b80be9567440e5319119d4284
Andrew Letson <velinath@users.noreply.github.com>
2017-05-10 06:21:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.content.indexOf('nervous man') > -1 || message.content.indexOf('end of trump\'s campaign') > -1 && message.channel.id === 272035227574992897) {
+  if (message.content.indexOf(' nervous man ') > -1 || message.content.indexOf(' end of trump\'s campaign ') > -1 && message.channel.id === 272035227574992897) {
     message.reply('http://www.theonion.com/article/will-be-end-trumps-campaign-says-increasingly-nerv-52002');
   }
 });
