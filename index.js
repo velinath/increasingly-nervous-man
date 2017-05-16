9e5316f96625308619a926aec46099b1180b63cd
Andrew Letson <velinath@users.noreply.github.com>
2017-05-16 11:27:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var onion_pattern = /(^|\s)(nervous man|end of trump's campaign)($|\s)/i
 var wh_live_pattern = /(^|\s)(today's disasters)($|\s)/i
 
 client.on('message', message => {
-  if(message.channel.id == 272035227574992897) {
+  if(message.channel.id == 272035227574992897 || message.channel.id == 311818566007652354) {
     if (onion_pattern.test(message.content)) {
       message.reply('<http://www.theonion.com/article/will-be-end-trumps-campaign-says-increasingly-nerv-52002>');
     } else if (wh_live_pattern.test(message.content)) {
@@ -23,8 +23,10 @@ client.on('message', message => {
           $('.view-content').filter(function() {
             var data = $(this);
             data.find('.views-row').each(function(i,v) {
-              var time = $(this).find('.date-display-single').text();
-              var event = $(this).find('a').text();
+              var time = $(this).find('.date-display-single').first().text();
+              var event = $(this).find('a').first().text();
+              console.log(time);
+              console.log(event);
               events.push(time + ': ' + event);
             });
           });
