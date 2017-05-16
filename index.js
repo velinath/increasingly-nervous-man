e8008d2989f20f2e21b35d5965fa157083337c2d
Andrew Letson <velinath@users.noreply.github.com>
2017-05-16 11:09:25 -0400

    today's disasters, maybe

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var request = require('request');
+var cheerio = require('cheerio');
 
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
 var onion_pattern = /(^|\s)(nervous man|end of trump's campaign)($|\s)/i
-var wh_live_pattern = /(^|\s)(man, today's disasters)($|\s)/i
+var wh_live_pattern = /(^|\s)(today's disasters)($|\s)/i
 
 client.on('message', message => {
   if(message.channel.id == 272035227574992897) {
     if (onion_pattern.test(message.content)) {
       message.reply('<http://www.theonion.com/article/will-be-end-trumps-campaign-says-increasingly-nerv-52002>');
+    } else if (wh_live_pattern.test(message.content)) {
+      var url = 'https://www.whitehouse.gov/live';
+      request(url, function(error, response, html) {
+        if(!error) {
+          var $ = cheerio.load(html);
+          var events = [];
+          $('.view-content').filter(function() {
+            var data = $(this);
+            data.find('.views-row').each(function(i,v) {
+              var time = $(this).find('.date-display-single').text();
+              var event = $(this).find('a').text();
+              events.push(time + ': ' + event);
+            });
+            message.reply(events.toString());
+      }
     }
   }
 });
