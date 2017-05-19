c7235ceb0ce4e6a9933a94513cf74f9d1a9b0653
Andrew Letson <velinath@users.noreply.github.com>
2017-05-19 12:07:06 -0400

    s/{/(/

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on('message', message => {
       message.reply('Who cares, nothing matters, no one knows anything, everything sucks.');
     } else if (sad_pattern.test(message.content)) {
       message.react(':sad:');
-    } else if {abuela_pattern.test(message.content)) {
+    } else if (abuela_pattern.test(message.content)) {
       message.react(':abuela:');
     } else if (wh_live_pattern.test(message.content)) {
       var url = 'https://www.whitehouse.gov/live';
