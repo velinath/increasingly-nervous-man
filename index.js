516f2db7dd9d296025fb93c299844530c9ac59ed
Andrew Letson <velinath@users.noreply.github.com>
2017-05-26 20:48:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var mattering_pattern = /(^|\s|\p)mattering/i
 var sad_pattern = /(^|\s)(sad!|low energy)/i
 var abuela_pattern = /(^|\s)(hillary|clinton)($|\s|\p)/i
 var daniels_pattern = /(^|\s)(voice friend bad|135b|but what if)($|\s|\p)/i
-var mlyp_pattern = /(^|\s|\p)(shameful|meaningless|garbage|fantastic|wonderful|perfect|sucks|awful|disgusting|terrible)($|\s|\p)/i
+var mlyp_pattern = /(^|\s|\p)(shameful|meaningless|garbage|fantastic|wonderful|perfect|sucks|awful|disgusting|terrible|unpleasant)($|\s|\p)/i
 
 client.on('message', message => {
   if(message.channel.id == 272035227574992897) {
