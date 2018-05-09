2635b88ea81f6222ec6a8d589aa45c2ae32f5a8b
Andrew Letson <velinath@users.noreply.github.com>
2018-05-09 18:11:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -187,7 +187,7 @@ client.on('message', message => {
   }
 });
 
-client.login(process.env.app_token).;
+client.login(process.env.app_token);
 var http = require('http');
 var finalhandler = require('finalhandler');
 var Router = require('router');
