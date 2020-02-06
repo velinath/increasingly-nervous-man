295710377e3ebad0b9d4768d6c80d411b88a74ef
Allison Letson <me@ajl.io>
2020-02-05 21:04:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -134,6 +134,26 @@ stream.on('tweet', function(tweet) {
   }
 }); 
 
+var previous_largest_known = 92;
+
+setTimeout(function() {
+  var percentage = previous_largest_known;
+  var url = 'https://results.thecaucuses.org';
+  rp(url)
+    .then(function(html) {
+      var $ = cheerio.load(html);
+      $('.view-content').filter(function() {
+        var data = $(this);
+        percentage = $(this).find('.is-centered"').first().text().split("%")[0];
+      });
+    });
+    if (percentage > previous_largest_known) {
+	  previous_largest_known = percentage;
+	  var channel = client.channels.get('272035227574992897');
+      message.channel.send(percentage);
+    }
+}, 120000);
+
 client.on('message', message => {
   if(channel_blacklist.indexOf(message.channel.id) === -1) {
     //general-use
