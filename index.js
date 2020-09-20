2e024b9adfcfcc6f504ef568ea7a8164b795a598
Allison Letson <me@ajl.io>
2020-09-20 13:36:35 -0400

    sort cards numerically

--- index.js
+++ index.js
@@ -121,6 +121,7 @@ var start_themind_round = function(game) {
       var random = Math.floor(Math.random() * cards.length);
       game.data.cards[player.id].push(parseInt(cards.splice(random,1)));
     }
+    game.data.cards[player.id].sort((a,b) => a-b);
     player.send("Your cards this round are `" + game.data.cards[player.id].toString() + "`. Play them with `!play <number>` in-channel.");
   });
   game.channel.send("Round " + game.data.round + " Start!")
